2c7841b8eef3d74c847703e8985a22d824f8fa02
Chunky <chunky@fluidtrends.com>
2025-01-17 17:14:52 +0100

    added the build

---
+++ build/assets/js/48387199.252e6683.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcarmeldocs=self.webpackChunkcarmeldocs||[]).push([[248],{6655:e=>{e.exports=JSON.parse('{"tags":[{"label":"announcements","permalink":"/news/tags/announcements","count":1},{"label":"releases","permalink":"/news/tags/releases","count":1}]}')}}]);
---
+++ build/assets/js/814f3328.5a4e6701.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcarmeldocs=self.webpackChunkcarmeldocs||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Private Beta Release 1","permalink":"/news/beta","unlisted":false,"date":"2025-01-17T00:00:00.000Z"}]}')}}]);
